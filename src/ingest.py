f946f9156dfa70019327ab9d90adda849cc337ab
Neda <neda.sultova@gmail.com>
2026-01-05 18:02:45 +0100

    added clean_txt()

--- src/ingest.py
+++ src/ingest.py
@@ -5,7 +5,11 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
 def clean_txt(txt:str) -> str:
-    pass
+    # remove things like impor-\ntant
+    txt = re.sub(r'-\n', '', txt)
+    # replase multiple newlines/tabs with single space
+    txt = re.sub(r'\s+', ' ', txt)
+    return txt.strip()
 
 def chunk_file(file_path:str) -> str:
     pass
