fa9714f11162b20e42d3600b25e189d2eec7b48b
Neda <neda.sultova@gmail.com>
2026-01-05 19:34:00 +0100

    added langchain-chroma to requirements.txt and added typechecking to vector.py

--- src/vector.py
+++ src/vector.py
@@ -1,8 +1,10 @@
+from typing import List
+from langchain_core.documents import Document
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from src.config import DB_DIR, EMBEDDING_MODEL_NAME
 
-def get_vector_db():
+def get_vector_db() -> Chroma:
     embedding_function = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
     
     return Chroma(
@@ -10,8 +12,7 @@ def get_vector_db():
         embedding_function=embedding_function
     )
 
-def add_documents_to_db(chunks):
+def add_documents_to_db(chunks: List[Document]) -> None:
     db = get_vector_db()
-    # Adding documents automatically generates embeddings
     db.add_documents(chunks)
-    print("Documents added to Vector DB.")
+    print(f"Successfully added {len(chunks)} chunks to Vector DB.")
