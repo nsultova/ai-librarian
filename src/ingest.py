71a8f7aec3465f1bf7d38981cb1df61d29084c6b
Neda <neda.sultova@gmail.com>
2026-01-05 17:58:15 +0100

    added skeletal structure for ingest

---
+++ src/ingest.py
@@ -0,0 +1,13 @@
+import re
+from typing import List
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredEPubLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+def clean_txt(txt:str) -> str:
+    pass
+
+def chunk_file(file_path:str) -> str:
+    pass
+
+
